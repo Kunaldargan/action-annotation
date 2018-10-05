a3f0858bd308b45a429950e94537abf3f56ba30c
Kunal Dargan <chakdekunal@gmail.com>
2018-10-05 16:20:10 +0530

    Create action-annotate.sh

---
+++ action-annotate.sh
@@ -0,0 +1,8 @@
+#Action-Annotate Requirements
+
+npm install electron electron-squirrel-startup electron-storage file-upload-thumbnail winston
+cd app/
+npm install jquery@latest
+npm i jQuery@latest
+cd ..
+npm i
